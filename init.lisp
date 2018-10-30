154e6364891e5c1fa890ad1ff6d08312bdf73123
Akhil Wali <akhil.wali.10@gmail.com>
2018-10-30 21:55:34 +1300

    Added more programs on startup

--- init.lisp
+++ init.lisp
@@ -14,10 +14,14 @@
   "Quit StumpWM, after making sure you really want to do that."
   (if surep (quit) t))
 
-(run-shell-command "feh --bg-fill ~/Dropbox/imgs/wallpapers/starf0rge.png")
 (run-shell-command "xmodmap /home/darth10/.xmodmap")
 (run-shell-command "xkbset accessx sticky -twokey latchlock")
 (run-shell-command "xkbset exp 1 '=accessx' '=sticky' '=twokey' '=latchlock'")
+
+(run-shell-command "feh --bg-fill ~/Dropbox/imgs/wallpapers/starf0rge.png")
+(run-shell-command "xscreensaver -no-splash")
+(run-shell-command "wicd-client -t")
+(run-shell-command "dropbox start")
 
 ;; set font
 (load-module "ttf-fonts")
