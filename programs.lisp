db4b93f574992488d73fefeed95b759883a61064
Akhil Wali <akhil.wali.10@gmail.com>
2021-07-21 14:05:11 +1200

    Migrating from Dropbox to pCloud

--- programs.lisp
+++ programs.lisp
@@ -4,11 +4,11 @@
 (run-shell-command "xkbset accessx sticky -twokey latchlock")
 (run-shell-command "xkbset exp 1 '=accessx' '=sticky' '=twokey' '=latchlock'")
 
-(run-shell-command "feh --bg-fill ~/Dropbox/imgs/wallpapers/starf0rge.png")
+(run-shell-command "feh --bg-fill ~/Cloud/imgs/wallpapers/starf0rge.png")
 (run-shell-command "xscreensaver -no-splash")
 (run-shell-command "xfce4-power-manager")
 (run-shell-command "nm-applet")
 (run-shell-command "xfce4-volumed")
 (run-shell-command "pasystray")
-(run-shell-command "dropbox start")
+(run-shell-command "/home/darth10/.local/bin/pcloud")
 (run-shell-command "blueman-applet")
