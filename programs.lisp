1d7402a724b7a5ec870cce680aac7e60a458e641
Akhil Wali <akhil.wali.10@gmail.com>
2023-02-07 11:07:28 +1300

    Use local wallpaper

--- programs.lisp
+++ programs.lisp
@@ -4,7 +4,7 @@
 (run-shell-command "xkbset accessx sticky -twokey latchlock")
 (run-shell-command "xkbset exp 1 '=accessx' '=sticky' '=twokey' '=latchlock'")
 
-(run-shell-command "feh --bg-fill ~/Cloud/imgs/wallpapers/starf0rge.png")
+(run-shell-command "feh --bg-fill ~/.local/lib/starf0rge.png")
 (run-shell-command "xscreensaver -no-splash")
 (run-shell-command "xfce4-power-manager")
 (run-shell-command "nm-applet")
